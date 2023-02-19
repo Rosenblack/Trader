1f08053b77c43f6fbd3e3f3185a864c413421cc0
Lucy <gakupo.gumi@gmail.com>
2023-02-19 14:43:36 +1300

    Init

---
+++ src/main/java/net/shale/horde/chem/Client.java
@@ -0,0 +1,10 @@
+package net.shale.horde.chem;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/shale/horde/chem/Main.java
@@ -0,0 +1,13 @@
+package net.shale.horde.chem;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main implements ModInitializer {
+	public static final String ID = "chemistry";
+	@Override
+	public void onInitialize() {
+	}
+}
